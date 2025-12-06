a1f62f17db64f9f4da8168cec68d4993096daace
Moti Bartov <moti.bartov@intenthq.com>
2025-12-06 17:32:35 +0200

    refactor: Remove BaseCubit and BaseBlocState
    Delete the `BaseCubit` and `BaseBlocState` files as they are no longer needed.

--- lib/presentation/base_cubit/base_cubit.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'base_bloc_state.dart';
-
-abstract class BaseCubit<S> extends Cubit<BaseBlocState<S>>{
-  BaseCubit(super.initialState);
-}
